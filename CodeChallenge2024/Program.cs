0505efb1b9ac613b76db8624a59464b562d500bc
Francesco Paolo Culcasi <f.culcasi@reply.it>
2024-03-21 10:12:34 +0100

    read file example

--- CodeChallenge2024/Program.cs
+++ CodeChallenge2024/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string INPUT_DIRECTORY = "../../../input/";
+string[] INPUT_FILES = { "a.txt" };
+string[] lines = File.ReadAllLines(INPUT_DIRECTORY + INPUT_FILES[0]);
+
+foreach (string line in lines)
+    Console.WriteLine(line);
